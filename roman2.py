270d2144ed76ed6ec00238bd163b1bb8271d7b6b
roman-ku <a>
2021-06-28 20:37:44 +0300

    more game bord upates

--- roman2.py
+++ roman2.py
@@ -3,23 +3,25 @@ from tkinter import *
 
 from tkinter import messagebox
 
-top = Tk()
+window = Tk()
 
 
-size = 50
+size = 55
 padding = 5
 
-C = Canvas(top, bg = "blue", height = (size * 8) + padding + padding, width = (size * 8) + padding + padding)
+C = Canvas(window, bg = "blue", height = (size * 8) + padding + padding, width = (size * 8) + padding + padding)
 
 # coord = 10, 50, 240, 210
 
+
 for x in range(8):
     for y in range(8):
+        fill_color = 'white' if (x + y) % 2 else '#8B4513' 
         start_x = (x * size) + padding
         start_y = (y * size) + padding
-        rect = C.create_rectangle(start_x, start_y, start_x + size, start_y + size, fill='white')
+        rect = C.create_rectangle(start_x, start_y, start_x + size, start_y + size, fill=fill_color, width=0)
 #arc = C.create_arc(coord, start = 0, extent = 150, fill = "red")
 #line = C.create_line(10,10,200,200,fill = 'white')
 
 C.pack()
-top.mainloop()
+window.mainloop()
